6a96cf9b5480b687ff74059a60cefb621ce70524
uihyun420 <uihyun420@gmail.com>
2025-09-12 17:59:33 +0900

    몬스터 웨이포인트 테스트

---
+++ Guardner/Assets/MonsterScript/WayPoint.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WayPoint : MonoBehaviour
+{
+    public MonoBehaviour monster;
+    private float speed = 1f;
+    public Transform[] wayPoint;
+    public int wayPointCount = 0;
+
+
+
+    private void Start()
+    {
+        transform.position = wayPoint[wayPointCount].transform.position;
+    }
+
+    private void Update()
+    {
+        MovePath();
+    }
+
+    public void MovePath()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint[wayPointCount].transform.position, speed * Time.deltaTime);
+        if(transform.position == wayPoint[wayPointCount].transform.position)
+        {
+            wayPointCount++;
+        }
+        if(wayPointCount == wayPoint.Length)
+        {
+            wayPointCount = 0;
+        }
+    }
+
+}
